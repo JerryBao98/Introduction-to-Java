f91c6126d72ee06141f96efe70d7c49d68dbf36b
destroyer8898 <a.bohaozhang17@gmail.com>
2023-09-09 17:04:56 -0600

    added name field to teacher class
    pls accept

--- src/people/Teacher.java
+++ src/people/Teacher.java
@@ -2,6 +2,16 @@ package people;
 
 public class Teacher {
     // name
+    private String name;
+    public Teacher(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     // subject
     // id
     // we may also have jr teachers?
