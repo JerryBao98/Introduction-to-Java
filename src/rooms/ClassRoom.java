4f631de291a5b89c883ccde37588a3a539ee5aea
Jerry Bao <jerrybao1998@outlook.com>
2023-09-09 16:22:46 -0600

    Added a students field to classroom

--- src/rooms/ClassRoom.java
+++ src/rooms/ClassRoom.java
@@ -1,9 +1,23 @@
 package rooms;
 
+import people.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassRoom {
 
     // has a list of students
     // also has a teacher
     // might? have a jr. teacher
 
+    private final List<Student> students;
+
+    public ClassRoom(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
 }
