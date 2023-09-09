e8ac4be8176e9b08dcd6658694111ac58986cf67
Austin Bao <austin_dun@outlook.com>
2023-09-09 17:32:04 -0600

    added subject field in teacher class

--- src/people/Teacher.java
+++ src/people/Teacher.java
@@ -2,7 +2,42 @@ package people;
 
 public class Teacher {
     // name
-    // subject
     // id
+    // subject
     // we may also have jr teachers?
+
+    private String name;
+    private String id;
+    private String subject;
+
+
+    public Teacher(String name, String id, String subject) {
+        this.name = name;
+        this.id = id;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
--- src/rooms/ClassRoom.java
+++ src/rooms/ClassRoom.java
@@ -1,5 +1,10 @@
 package rooms;
 
+import people.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassRoom {
 
     // has a list of students
@@ -7,4 +12,13 @@ public class ClassRoom {
     // might? have a jr. teacher
 
  
+    private final List<Student> students;
+
+    public ClassRoom(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
 }
