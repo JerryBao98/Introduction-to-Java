2b385ec46d2a3b868fbca48a9c5f0605ac29b9ad
GithubConnoisseur <galileokim451@gmail.com>
2023-09-09 16:43:02 -0600

    Added a field for a students name.

--- src/people/Student.java
+++ src/people/Student.java
@@ -3,5 +3,14 @@ package people;
 public class Student {
     // name
     // grade
+    private final String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
